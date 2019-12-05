dee17b1496ec0a7d0a447c59468af5bcc243ece5
Serhii Babii <babiysergey1988@gmail.com>
2019-12-05 14:19:51 +0200

    HW7-01 + connect(ModalItem.jsx)

--- src/components/ModalItem/ModalItem.jsx
+++ src/components/ModalItem/ModalItem.jsx
@@ -2,22 +2,24 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import {addTodoItem, hideModalArea} from '../../actions'
 
 import './ModalItem.css';
 
 const ModalItem = ({
-  addItem,
-  clearModalField,
-  modalShow,
+  addItem1,
+  modalShow1,
   currentId,
   currentTitle,
   currentDescription,
   currentPriority,
   handleSelect,
+  hideModalArea1,
 }) => {
   return (
-    <section className={modalShow ? 'visible' : 'invisible'}>
-      <div className="modal__fade" onClick={clearModalField} />
+    <section className={modalShow1 ? 'visible' : 'invisible'}>
+      <div className="modal__fade" onClick={hideModalArea1} />
       <div className="modal__form">
         <div className="modal__part">
           <label htmlFor="title">
@@ -57,13 +59,13 @@ const ModalItem = ({
           </label>
         </div>
         <div className="modal__buttons">
-          <button type="button" className="btn btn-warning" onClick={clearModalField}>
+          <button type="button" className="btn btn-warning" onClick={hideModalArea1}>
             Cancel
           </button>
           <button
             type="button"
             className="btn btn-success"
-            onClick={() => addItem(currentTitle, currentDescription, currentPriority, currentId)}
+            onClick={() => addItem1(currentTitle, currentDescription, currentPriority, currentId)}
           >
             Save
           </button>
@@ -73,22 +75,28 @@ const ModalItem = ({
   );
 };
 
-ModalItem.defaultProps = {
-  currentId: '',
-}
-
 ModalItem.propTypes = {
-  addItem: PropTypes.func.isRequired,
-  clearModalField: PropTypes.func.isRequired,
-  modalShow: PropTypes.bool.isRequired,
-  currentId: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
+  addItem1: PropTypes.func.isRequired,
+  modalShow1: PropTypes.bool.isRequired,
+  currentId: PropTypes.string.isRequired,
   currentTitle: PropTypes.string.isRequired,
   currentDescription: PropTypes.string.isRequired,
   currentPriority: PropTypes.string.isRequired,
   handleSelect: PropTypes.func.isRequired,
+  hideModalArea1: PropTypes.func.isRequired,
 }
 
-export default ModalItem;
+const mapStateToProps = (state) => {
+  return {
+    modalShow1: state.todos.modalShow,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addItem1: (title, description, priority) => dispatch(addTodoItem(title, description, priority)),
+    hideModalArea1: () => dispatch(hideModalArea()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalItem);
