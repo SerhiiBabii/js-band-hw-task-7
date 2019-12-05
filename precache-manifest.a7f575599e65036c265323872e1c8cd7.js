d9dc19b5af520b193518fcb96da56c3285a528b4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-05 11:52:58 +0000

    Deploy SerhiiBabii/js-band-hw-task-7 to github.com/SerhiiBabii/js-band-hw-task-7.git:gh-pages

---
+++ precache-manifest.a7f575599e65036c265323872e1c8cd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c9546ea32084b50906b884542daeb9",
+    "url": "/js-band-hw-task-7/index.html"
+  },
+  {
+    "revision": "68ede0c1eb6bdc5182bc",
+    "url": "/js-band-hw-task-7/static/css/main.08194591.chunk.css"
+  },
+  {
+    "revision": "b8be4a8450b59b0993eb",
+    "url": "/js-band-hw-task-7/static/js/2.0954e946.chunk.js"
+  },
+  {
+    "revision": "68ede0c1eb6bdc5182bc",
+    "url": "/js-band-hw-task-7/static/js/main.b1256482.chunk.js"
+  },
+  {
+    "revision": "82db8f84bbcc003ba0b0",
+    "url": "/js-band-hw-task-7/static/js/runtime-main.9cc15563.js"
+  }
+]);
